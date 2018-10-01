883d7d8bd61d37ff9959542e59b620afd2b45ff5
Nowakowski <mjnowakowski@labs.neit.edu>
2018-10-01 13:59:56 -0400

    tests and stuff

--- app.js
+++ app.js
@@ -4,23 +4,34 @@ const jae = require("jae-engine");
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-	response.on('error', (err) => {
-		console.error(err);
-	});
+	console.log("Request recieved");
 
 	response.setHeader('Access-Control-Allow-Origin', '*');
 	if(request.method === 'OPTIONS') {
+		console.log("Pre-flight");
 		response.setHeader('Access-Control-Allow-Methods', 'POST');
 		response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 		response.writeHead(200);
 		response.end();
-	} else if (request.method === 'POST') {
-		response.setHeader('Content-Type', request.headers["content-type"]);
-		response.writeHead(200);
-		request.pipe(response);
-	} else {
-		response.setHeader("Content-Type", "text/plain");
-		response.writeHead(403);
-		response.end("Please request using POST");
+		return;
 	}
+
+	response.on('error', (err) => {
+		console.error(err);
+	});
+
+	request.on('data', (chunk) => {
+		console.log("Data read: " + chunk);
+	}).on('end', () => {
+		console.log("Respond");
+		if (request.method === 'POST') {
+			response.setHeader("Content-Type", "text/plain");
+			response.writeHead(200);
+			response.end("test");
+		} else {
+			response.setHeader("Content-Type", "text/plain");
+			response.writeHead(403);
+			response.end("Control requests should be sent through POST");
+		}
+	});
 }).listen(settings.app.port);
