27fca624d9f1686457ae3fe9aef3424d2db6ca97
= <=>
2018-10-02 21:04:56 -0400

    stuff

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const { settings, env } = require("./config.js");
 
-const jae = require("jae-engine");
+const JAE = require("jae-engine");
 const http = require("http");
 
 const server = http.createServer((request, response) => {
 	console.log("Request recieved");
+	let reqData;
 
 	response.setHeader('Access-Control-Allow-Origin', '*');
 	if(request.method === 'OPTIONS') {
@@ -22,12 +23,23 @@ const server = http.createServer((request, response) => {
 
 	request.on('data', (chunk) => {
 		console.log("Data read: " + chunk);
+		switch(request.headers['content-type']) {
+			case "application/json":
+				try { reqData = JSON.parse(chunk);
+				} catch(err) { reqData = null; }
+				console.log(reqData);
+				break;
+			default:
+				break;
+		}
+		
 	}).on('end', () => {
 		console.log("Respond");
 		if (request.method === 'POST') {
 			response.setHeader("Content-Type", "text/plain");
 			response.writeHead(200);
 			response.end("test");
+			return;
 		} else {
 			response.setHeader("Content-Type", "text/plain");
 			response.writeHead(403);
